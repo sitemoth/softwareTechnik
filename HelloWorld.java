97e3ff42ad2933f26615bd62832c8f6fa84b387f
Emil Baumotte <baumoemi@localhost.localdomain>
2020-11-05 15:33:58 +0100

    pushed not functioning hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,24 @@
+package java.time;
+
 class HelloWorld
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World." );
+        System.out.println( "haha jk" );
+
+        // DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MM yyyy");
+        // String inputString1 = "01 04 2020";
+        // String inputString2 = "05 11 2020";
+        
+        LocalDateTime tStart;
+        LocalDateTime tNow;
+
+        tStart = LocalDateTime.parse( "2020-04-01T00:00:00" );
+        tNow = LocalDateTime.now();
+
+        long minutesBetween = Duration.between( tStart, tNow ).toMinutes();
+
+        System.out.println( "MINUTES SINCE STUDY START " + minutesBetween );
     }
 }
